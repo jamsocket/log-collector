5cdd0ab4a85e730e204868a031bd41b99f6a7471
Paul Butler <paul@driftingin.space>
2024-04-17 14:11:53 -0400

    improvements

--- src/sink.rs
+++ src/sink.rs
@@ -1,7 +1,7 @@
 use crate::types::LogMessage;
 use anyhow::Result;
-use futures_util::SinkExt;
-use tokio::sync::mpsc::Receiver;
+use futures_util::{SinkExt, StreamExt};
+use tokio::{select, sync::mpsc::Receiver};
 use tokio_tungstenite::tungstenite::Message;
 
 enum Impossible {}
@@ -10,25 +10,32 @@ async fn run_socket(url: &str, receiver: &mut Receiver<LogMessage>) -> Result<Im
     let (mut conn, _) = tokio_tungstenite::connect_async(url).await?;
 
     loop {
-        let msg = receiver.recv().await;
-        if let Some(msg) = msg {
-            let msg_str = serde_json::to_string(&msg)?;
-            let msg = Message::Text(msg_str);
-            let _ = conn.send(msg).await;
-        } else {
-            return Err(anyhow::anyhow!("Receiver closed"));
+        select! {
+            msg = receiver.recv() => {
+                if let Some(msg) = msg {
+                    let msg_str = serde_json::to_string(&msg)?;
+                    let msg = Message::Text(msg_str);
+                    let _ = conn.send(msg).await;
+                } else {
+                    return Err(anyhow::anyhow!("Receiver closed"));
+                }
+            },
+            incoming = conn.next() => {
+                if incoming.is_none() {
+                    return Err(anyhow::anyhow!("Connection closed"));
+                }
+            }
         }
     }
 }
 
 pub async fn forward_to_socket(url: String, mut receiver: Receiver<LogMessage>) {
     loop {
+        tracing::info!("Attempting socket connection.");
         if let Err(err) = run_socket(&url, &mut receiver).await {
-            println!("Error: {}", err);
+            tracing::warn!("Error: {}", err);
         }
 
         tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     }
 }
-
-
